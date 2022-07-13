1aae1a3ec0220b0df3a4bc2c8c94aa1b0c852b6c
DamilolaF <dami_fagbo@yahoo.com>
2022-07-13 18:16:45 +0100

    update
    update

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return <main>
+    <div className="container">
+      <h3></h3>
+    </div>
+  </main>;
 }
 
 export default App;
