c5049da103a453bd0df826bed7dabf12b57c8eec
DamilolaF <dami_fagbo@yahoo.com>
2022-07-13 17:12:16 +0100

    dami_fagbo@yahoo.com
    update

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -6,8 +6,35 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index]
   
-  
-  
+const checkNumber = (number) => {
+  if (number > people.length -1) {
+      return 0;
+  }
+  if (number < 0) {
+    return people.length - 1;
+  }
+  return number;
+}
+
+  const nextPerson  = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson  = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+const randomPerson = () => {
+let randomNumber = Math.floor(Math.random() * people.length);
+if (randomNumber === index) {
+  randomNumber = index + 1;
+}
+setIndex(checkNumber(randomNumber));
+}
   return <article className='review'> 
   <div className="img-container">
     <img src={image} alt={name} className='person-img' />
@@ -25,10 +52,10 @@ const Review = () => {
     <button className='next-btn' onClick={nextPerson}>
     <FaChevronRight />
     </button>
-    <button className='random-btn'>
+  </div>
+  <button className='random-btn' onClick={randomPerson}>
     surprise me
     </button>
-  </div>
   </article>
   
 };
