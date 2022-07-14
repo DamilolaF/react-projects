6274baf5c4df3e6a55e47bb6224c07cfad0f30f2
DamilolaF <dami_fagbo@yahoo.com>
2022-07-14 12:55:59 +0100

    commit updated

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -5,7 +5,13 @@ function App() {
   const [questions, setQuestions] = useState(data);
   return <main>
     <div className="container">
-      <h3></h3>
+      <h3>Questions and answers about login</h3>
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id}
+          {...question} />;
+        })}
+      </section>
     </div>
   </main>;
 }
