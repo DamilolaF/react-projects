e23de00c468f49452fe12bf8513d2bebaf0788b2
DamilolaF <dami_fagbo@yahoo.com>
2022-07-18 14:16:03 +0100

    update

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -20,12 +20,37 @@ function App() {
  }, []);
 
  if(loading){
-   return <section className='section loading'>
+   return  (
+   <section className='section loading'>
      <h1>loading...</h1>
    </section>
- };
-  
-  return <h2>jobs</h2>
+   );
+ }
+  const {company, dates, duties,title} =jobs[value];
+  return <section className='section'>
+    <div className="title">
+      <h2>experience</h2>
+      <div className="underline"></div>
+    </div>
+    <div className="jobs-center">
+      { /*btn container*/ }
+      {/* job info */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className="job-desc">
+              <FaAngleDoubleRight 
+              className='job-icon'></FaAngleDoubleRight>
+           <p>{duty}</p>
+            </div>
+            );
+        })}
+      </article>
+    </div>
+  </section>
 }
 
 export default App
