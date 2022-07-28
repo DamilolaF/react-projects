6dc27781313d0798ae30919a7f8019db1749574c
DamilolaF <dami_fagbo@yahoo.com>
2022-07-28 15:15:11 +0100

    update

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,20 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  }
+  else {
+    return []
+  }
+}
+
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -21,9 +32,18 @@ const handleSubmit = (e) => {
     }
     else if(name && isEditing){
       //deal with edit
-
-    }
-  else {
+        setList(list.map((item)=>{
+        if(item.id === editID){
+          return{...item, title:name }
+        }  
+        return item
+          })
+        )
+        setName('');
+        setEditID(null);
+        setIsEditing(false);
+        showAlert(true, 'success', 'value changed')
+    } else {
     showAlert(true, 'success', 'item added to the list')
     const newItem= {id: new Date().getTime().toString(),
     title: name}
@@ -51,6 +71,9 @@ const editItem = (id) => {
   setEditID(id)
   setName(specificItem.title)
 }
+useEffect(()=>{
+  localStorage.setItem('list', JSON.stringify(list))
+},[list])
   return (
   <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
