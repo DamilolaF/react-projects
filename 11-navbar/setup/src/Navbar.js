60540444d6099360b3372981d6641a186a15b4d8
DamilolaF <dami_fagbo@yahoo.com>
2022-08-01 21:34:42 +0100

    update

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,32 +1,45 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(false)
-  return <nav>
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if(showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }  
+  }, [showLinks]);
+  
+  return ( 
+  <nav>
     <div className="nav-center">
       <div className="nav-header">
-        <img src={logo} alt="logo" />
-        <button className='nav-toggle' onClick={()=>
-       setShowLinks(!showLinks)}>
+        <img src={logo} className='logo'  alt="logo" />
+        <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      {showLinks && ( 
-        <div className={`${showLinks ? 'links-container show-container':
-        'links-container'}`}>
-          <ul className='links'>
+      
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
                 const {id,url,text} = link;
-                return <li key={id}>
+                return ( 
+                <li key={id}>
                     <a href={url}>{text}</a>
-                </li>   
+                </li>  
+                ) 
             })}
           </ul>
         </div>
-      )}
+     
         <ul className='social-icons'>
         {social.map((socialIcon)=>{
           const{id,url,icon} = socialIcon;
@@ -37,6 +50,7 @@ const Navbar = () => {
         </ul>
     </div>
   </nav>
+  )
 }
 
 export default Navbar
