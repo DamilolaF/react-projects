ae30cf291594a8493a35cb26a180db0d9ff0cdb5
DamilolaF <dami_fagbo@yahoo.com>
2022-08-08 15:38:27 +0100

    update

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -8,8 +8,8 @@ const Hero = () => {
   return <section className='hero'>
     <div className="hero-center">
       <article className="hero-info">
-        <h1>Payments infrastructure
-            for the internet</h1>
+        <h2>Payments infrastructure
+            for the internet</h2>
         <p>
         Millions of companies of all sizes—from startups to Fortune 500s—use
         Stripe’s software and APIs to accept payments, send payouts, and
